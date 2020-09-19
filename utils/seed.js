99a6b846258bf54e936c3f23e85c789c507cb90a
JasonEb <jasonebueng@gmail.com>
2020-09-18 21:44:51 -0700

    refactors seed.js

--- utils/seed.js
+++ utils/seed.js
@@ -6,46 +6,26 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const _ = require("lodash");
 
+const { ObjectId } = mongoose.Types
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then((conn) => {
-    const userInsert = async (users) => {
-      await User.insertMany(users)
-        .then((e) => console.log("User seed success"))
-        .catch((err) => console.log("User Seed Error", err));
-    };
-    
-    const populateTweets = async () => {
-      // create tweets by getting random user ids assigned to newTweets
-      await User.find({}).then((users) => {
-        let tweets = [];
-        for (let x = 0; x < 10; x++) {
-          const newTweet = new Tweet({
-            text: faker.lorem.sentence(),
-            user: _.sample(users.map((user) => user._id)),
-          });
-          tweets.push(newTweet);
-        }
-        return Tweet.insertMany(tweets);
-      });
-    };
-    
     let users = [];
     let newUser = {};
+    let tweets = [];
 
     console.log(`Seed file connected to MongoDB successfully`);
 
     // WARNING! This code below will delete the database. 
     // Uncomment to enable resetting the database each time the script is ran.
-
-    /* 
     conn.connection.db.dropDatabase(
       console.log(`${conn.connection.db.databaseName} database dropped.`)
     );
-    */
-
+    
     for (let x = 0; x < 10; x++) {
       newUser = {
+        _id: new ObjectId(x),
         username: faker.internet.userName(),
         email: faker.internet.email(),
         password: bcrypt.hashSync("test123", 10),
@@ -53,11 +33,17 @@ mongoose
       users.push(newUser);
     }
 
-    userInsert(users).then(() => {
-      populateTweets().then(() => {
-        console.log("Tweets populated");
-        mongoose.connection.close();
+    for (let y = 0; y < 100; y++) {
+      const newTweet = new Tweet({
+        _id: new ObjectId(y),
+        text: faker.lorem.sentence(),
+        user: _.sample(users.map((user) => user._id)),
       });
-    })
+      tweets.push(newTweet);
+    } 
+
+    let promises = [ User.insertMany(users), Tweet.insertMany(tweets) ]
+
+    Promise.all(promises).then ( () => mongoose.connection.close() )
   })
   .catch((err) => console.log(err));
